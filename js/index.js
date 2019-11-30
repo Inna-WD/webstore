1c60f01cf0403780e3c44b3f40bc7d1c515f91c1
Inna-WD <inna.druker@gmail.com>
2019-11-30 17:00:28 -0500

    30/11

---
+++ js/index.js
@@ -0,0 +1,293 @@
+const allProducts = [
+    {
+        id: `bp1`,
+        name: `Black tourmaline`,
+        desc: `Inlay chip, black tourmaline (natural), mini undrilled chip, Mohs hardness 7 to 7-1/2. Sold per 50-gram pkg, approximately 550-700 pieces.`,
+        color: `black`
+        material: `stone`,
+        sold by: `weight`,
+        price: 19.99,
+        quantity: 12,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `bp2`,
+        name: `Black onyx`,
+        desc: `Bead, black onyx (dyed), 8mm round, A- grade, Mohs hardness 6-1/2 to 7. Sold per 15-1/2" to 16" strand`,
+        color: `black`
+        material: `stone`,
+        sold by: `strand`,
+        price: 24.99,
+        quantity: 25,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `bp3`,
+        name: `Lava rock`,
+        desc: `Bead, lava rock (waxed), 8mm round with 0.5-1.5mm hole, B grade, Mohs hardness 3 to 3-1/2. Sold per 15-1/2" to 16" strand.`,
+        color: `black`
+        material: `stone`,
+        sold by: `strand`,
+        price: 29.99,
+        quantity: 11,
+        category: `natural`,
+        favorite: no
+    }, {
+        id: `bp4`,
+        name: `Obsidian `,
+        desc: `Bead, black obsidian (natural), 8mm round, C grade, Mohs hardness 5 to 5-1/2. Sold per pkg of (10) 15-1/2" to 16" strands.`,
+        color: `black`
+        material: `stone`,
+        sold by: `strand`,
+        price: 29.99,
+        quantity: 11,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `bp5`,
+        name: `Agate`,
+        desc: `Bead, black agate (dyed), 26x17mm-32x23mm freeform with 0.5-1.5mm hole, B grade, Mohs hardness 6-1/2 to 7. Sold per 8-inch strand, approximately 5 beads.`,
+        color: `Agate`
+        material: `stone`,
+        sold by: `strand`,
+        price: 34.99,
+        quantity: 5,
+        category: `natural`,
+        favorite: no
+    }, {
+        id: `wp1`,
+        name: `Magnesite`,
+        desc: `Focal, magnesite (stabilized), white, 20x5mm-39x5mm graduated spike fan, B grade, Mohs hardness 3-1/2 to 4. Sold per 10-piece set.`,
+        color: `white`
+        material: `glass`,
+        sold by: `item`,
+        price: 19.99,
+        quantity: 43,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `wp2`,
+        name: `Selenite`,
+        desc: `Bead, selenite (natural), mini to small hand-cut undrilled chip, Mohs hardness 2 to 2-1/2. Sold per 2-ounce pkg.`,
+        color: `white`
+        material: `stone`,
+        sold by: `weight`,
+        price: 24.99,
+        quantity: 4,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `wp3 `,
+        name: `Howlite`,
+        desc: `Bead, white howlite (natural), 8mm round, B grade, Mohs hardness 3 to 3-1/2. Sold per 15-1/2" to 16" strand.`,
+        color: `white`
+        material: `stone`,
+        sold by: `strand`,
+        price: 29.99,
+        quantity: 5,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `wp4`,
+        name: `Howlite`,
+        desc: `Bead, white howlite (natural), 6mm round, C grade, Mohs hardness 3 to 3-1/2. Sold per pkg of (10) 15-1/2" to 16" strands.`,
+        color: `white`
+        material: `stone`,
+        sold by: `strand`,
+        price: 34.99,
+        quantity: 45,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `wp5`,
+        name: `Czech glass`,
+        desc: `Bead, Czech pressed glass, opaque white, 9.5x3mm ring with 3.5mm hole. Sold per pkg of 50.`,
+        color: `white`
+        material: `glass`,
+        sold by: `item`,
+        price: 39.99,
+        quantity: 21,
+        category: `plastic`,
+        favorite: false
+    }, {
+        id: `gp1`,
+        name: `Delica`,
+        desc: `Seed bead, Delica�, glass, Duracoat� opaque galvanized silver, (DB1831), #11 round. Sold per 7.5-gram pkg.`,
+        color: `grey`
+        material: `glass`,
+        sold by: `weight`,
+        price: 19.99,
+        quantity: 3,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `gp2`,
+        name: `Cheesewood`,
+        desc: `Bead, painted Taiwanese cheesewood (coated), metallic silver, 19-20mm round with 2.5-3mm hole. Sold per 15-1/2" to 16" strand.`,
+        color: `grey`
+        material: `glass`,
+        sold by: `strand`,
+        price: 24.99,
+        quantity: 69,
+        category: `plastic`,
+        favorite: false
+    }, {
+        id: `gp3`,
+        name: `Bead`,
+        desc: `Bead, acrylic, silver, 8mm round with stars, 1.75mm hole. Sold per pkg of 100.`,
+        color: `grey`
+        material: `acrylic`,
+        sold by: `item`,
+        price: 29.99,
+        quantity: 5,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `gp4`,
+        name: `Bead mix`,
+        desc: `Bead mix, acrylic, opaque silver and black, 7mm double-sided flat round with alphabet letters. Sold per pkg of 200.`,
+        color: `grey`
+        material: `acrylic`,
+        sold by: `item`,
+        price: 34.99,
+        quantity: 7,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `gp5`,
+        name: `Bead crystals`,
+        desc: `Bead, Swarovski� crystals, crystal silver night, 4mm Xilion bicone (5328). Sold per pkg of 48.`,
+        color: `grey`
+        material: `glass`,
+        sold by: `item`,
+        price: 39.99,
+        quantity: 8,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `blp1`,
+        name: `Lampworked glass `,
+        desc: `Bead, lampworked glass, multicolored, 22mm double-sided puffed flat round with heart pattern. Sold per pkg of 2.`,
+        color: `blue`
+        material: `glass`,
+        sold by: `item`,
+        price: 19.99,
+        quantity: 54,
+        category: `plastic`,
+        favorite: false
+    }, {
+        id: `blp2`,
+        name: `Carpi blue`,
+        desc: `Bead, Swarovski� crystals, Capri blue, 6mm Xilion bicone (5328). Sold per pkg of 24.`,
+        color: `blue`
+        material: `glass`,
+        sold by: `item`,
+        price: 24.99,
+        quantity: 14,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `blp3`,
+        name: `Lapis lazuli`,
+        desc: `Bead, lapis lazuli (natural), medium chip, Mohs hardness 5 to 6. Sold per 36-inch strand.`,
+        color: `blue`
+        material: `stone`,
+        sold by: `strand`,
+        price: 29.99,
+        quantity: 45,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `blp4`,
+        name: `Celestial glass`,
+        desc: `Bead, Celestial Crystal�, transparent turquoise blue, 10x3.5mm / 8x3mm / 6x2mm margarita flower. Sold per pkg of 12.`,
+        color: `blue`
+        material: `glass`,
+        sold by: `item`,
+        price: 34.99,
+        quantity: 12,
+        category: `plastic`,
+        favorite: false
+    }, {
+        id: `blp5`,
+        name: `Turquoise glass`,
+        desc: `Bead mix, lampworked glass, turquoise blue with fancy finish, 7x4mm-14x11mm mixed shapes. Sold per pkg of 100-grams, approximately 60-100 beads.`,
+        color: `blue`
+        material: `acrylic`,
+        sold by: `weight`,
+        price: 39.99,
+        quantity: 23,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `rp1 `,
+        name: `Multicolored glass`,
+        desc: `Bead, lampworked glass, multicolored, 28x17mm-31x17mm cardinal with folded wings and 2.6mm hole. Sold per pkg of 2.`,
+        color: `red`
+        material: `acrylic`,
+        sold by: `item`,
+        price: 19.99,
+        quantity: 87,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `rp2`,
+        name: `Inlay chip  `,
+        desc: `Inlay chip, "coral" (resin) (imitation), red, mini undrilled chip. Sold per 50-gram pkg, approximately 550-700 pieces.`,
+        color: `red`
+        material: `stone`,
+        sold by: `weight`,
+        price: 24.99,
+        quantity: 12,
+        category: `acrylic`,
+        favorite: false
+    }, {
+        id: `rp3`,
+        name: `Siam`,
+        desc: `Bead, Swarovski� crystals, Siam, 10mm faceted heart (5742). Sold per pkg of 2.`,
+        color: `red`
+        material: `glass`,
+        sold by: `item`,
+        price: 29.99,
+        quantity: 45,
+        category: `natural`,
+        favorite: false
+    }, {
+        id: `rp4`,
+        name: `Pewter`,
+        desc: `Bead, crystal and antique silver-plated "pewter" (zinc-based alloy), transparent red and clear, 23mm angel. Sold per pkg of 4.`,
+        color: `red`
+        material: `glass`,
+        sold by: `item`,
+        price: 34.99,
+        quantity: 54,
+        category: `plastic`,
+        favorite: false
+    }, { 
+        id: `rp5`,
+        name: `Santa`,
+        desc: `Bead, lampworked glass, white and red, 23x14mm Santa. Sold per pkg of 2.`,
+        color: `red`
+        material: `glass`,
+        sold by: `item`,
+        price: 39.99,
+        quantity: 65,
+        category: `natural`,
+        favorite: false
+    }
+];
+
+const getProductAsHtmlString = (product) => {
+    return `
+      <article class="product">
+        <h3 >${product.name}</h3>
+        <ul>
+          <li>Product Id: <strong>${product.id}</strong></li>
+          <li>Product description: <strong>${product.desc}</strong></li>
+          <li>Color: <strong>${product.color} </strong></li>
+          <li>Material: <strong>${product.material} </strong></li>
+        </ul>
+      </article>`;
+}
+      document.getElementById('products').innerHTML = allProducts.map(getProductAsHtmlString).join('\n')
+}
